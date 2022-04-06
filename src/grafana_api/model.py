1a8291408e9d2ab302fcc3ca7e4d68a46559f097
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2022-04-06 08:45:38 +0200

    Add basic organisation support

--- src/grafana_api/model.py
+++ src/grafana_api/model.py
@@ -22,6 +22,7 @@ class APIEndpoints(Enum):
     DATASOURCE_QUERY = "/api/tsdb/query"
     SHORT_URLS = "/api/short-urls"
     ORGANISATION = "/api/org"
+    ORGANISATIONS = "/api/orgs"
 
 
 class RequestsMethods(Enum):
@@ -30,6 +31,7 @@ class RequestsMethods(Enum):
     GET = "GET"
     PUT = "PUT"
     POST = "POST"
+    PATCH = "PATCH"
     DELETE = "DELETE"
 
 
@@ -43,6 +45,20 @@ class APIModel(NamedTuple):
 
     host: str
     token: str
+
+
+class APIBasicModel(NamedTuple):
+    """The class includes all necessary variables to establish a basic connection to the Grafana API endpoints
+
+    Args:
+        host (str): Specify the host of the Grafana system
+        username (str): Specify the username of the Grafana system
+        password (str): Specify the password of the Grafana system
+    """
+
+    host: str
+    username: str
+    password: str
 
 
 class DatasourceQuery(NamedTuple):
