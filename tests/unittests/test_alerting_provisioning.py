f7cbaaab724b5a1422466f06f2663c1126a3a532
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2023-02-22 06:24:02 +0100

    WIP

--- tests/unittests/test_alerting_provisioning.py
+++ tests/unittests/test_alerting_provisioning.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
-
-from grafana_api.model import APIModel
-from grafana_api.alerting_provisioning import AlertingProvisioning
+# from unittest.mock import MagicMock, patch
+#
+# from grafana_api.model import APIModel
+# from grafana_api.alerting_provisioning import AlertingProvisioning
 
 
 class AlertingProvisioningTestCase(TestCase):
