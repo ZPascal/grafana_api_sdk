b52b11f464e47dc834a865a2fd6b8eeadd04bfa4
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2023-08-05 22:14:34 +0200

    fix: Update the release version (#38)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = long_description.replace(coverage_string, "")
 
 setuptools.setup(
     name="grafana-api-sdk",
-    version="0.2.0",
+    version="0.3.0",
     author="Pascal Zimmermann",
     author_email="info@theiotstudio.com",
     description="A Grafana API SDK",
